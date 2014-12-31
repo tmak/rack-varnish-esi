337949d360e8e5bae25d9a050222a13b5b20ae05
Thomas Marek <thomas_marek@gmx.net>
2014-12-31 10:27:49 +0100

    Initial commit with "bundle gem --test=minitest"

---
+++ lib/rack/varnish/esi.rb
@@ -0,0 +1,9 @@
+require "rack/varnish/esi/version"
+
+module Rack
+  module Varnish
+    module Esi
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rack/varnish/esi/version.rb
@@ -0,0 +1,7 @@
+module Rack
+  module Varnish
+    module Esi
+      VERSION = "0.0.1"
+    end
+  end
+end
